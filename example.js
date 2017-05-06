76202f87e3ab0bb87981d9516644ed1256474edf
vokoshyv <ronsuij@gmail.com>
2017-05-06 09:46:19 -0700

    (feat) will come back to this in some time when async and await become keywords

---
+++ example.js
@@ -0,0 +1,12 @@
+const rp = require('request-promise');
+const fetch = require('node-fetch');
+
+async function showGitHubUser(handle){
+  const url = `https://api.github.com/users/${handle}`;
+  const response = await fetch(url);
+  const user = await response.json();
+  console.log(user.name);
+  console.log(user.location);
+}
+
+console.log(showGitHubUser('vokoshyv'));
